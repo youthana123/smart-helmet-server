82eea53d5e5b60cdbe92cc58e6a8ab655e6b3577
youthana123 <yootana2545@gmail.com>
2026-06-27 22:42:35 +0700

    Refactor server.js for improved WebSocket handling

--- server.js
+++ server.js
@@ -20,22 +20,19 @@ wss.on('connection', (ws) => {
             
             if (data.type === 'register') {
                 clients.set(ws, { role: data.role, deviceId: data.deviceId || 'boss' });
-            } 
-            else if (data.type === 'sensor_data') {
+            } else if (data.type === 'sensor_data') {
                 clients.forEach((info, clientWs) => {
                     if (info.role === 'boss' && clientWs.readyState === WebSocket.OPEN) {
                         clientWs.send(JSON.stringify(data));
                     }
                 });
-            }
-            else if (data.type === 'ack_sos') {
+            } else if (data.type === 'ack_sos') {
                 clients.forEach((info, clientWs) => {
                     if (info.role === 'helmet' && info.deviceId === data.targetId && clientWs.readyState === WebSocket.OPEN) {
                         clientWs.send(JSON.stringify({ type: 'ack_sos' }));
                     }
                 });
-            }
-            else if (data.type === 'set_worker') {
+            } else if (data.type === 'set_worker') {
                 clients.forEach((info, clientWs) => {
                     if ((info.role === 'boss' || (info.role === 'helmet' && info.deviceId === data.deviceId)) 
                         && clientWs.readyState === WebSocket.OPEN) {
@@ -48,9 +45,7 @@ wss.on('connection', (ws) => {
         }
     });
 
-    ws.on('close', () => {
-        clients.delete(ws);
-    });
+    ws.on('close', () => clients.delete(ws));
 });
 
 const PORT = process.env.PORT || 3000;
